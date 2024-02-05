af224910f980a5e7ae8603081adf651693cab457
Sebastian Werner <s.werner@sebastian-software.de>
2024-02-05 22:04:23 +0100

    feat: created imprint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      assetsInlineLimit: 512,
+    },
+    plugins: [vanillaExtractPlugin()],
+  },
+});
