502d6225e6155b98515cc2c5ed89f5423c39798e
Sebastian Werner <s.werner@sebastian-software.de>
2024-02-05 22:48:28 +0100

    feat: added web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   vite: {
     build: {
       assetsInlineLimit: 512,
