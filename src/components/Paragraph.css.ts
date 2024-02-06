72f63f5391c709213cd15f189593c48e3bced674
Sebastian Werner <s.werner@sebastian-software.de>
2024-02-06 15:18:28 +0100

    feat: tweaked styles and added ssoft logo

---
+++ src/components/Paragraph.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css"
+
+export const rootClass = style({
+  fontSize: "0.875rem",
+  marginBottom: "1rem",
+  maxWidth: "60ch"
+})
--- src/layouts/Layouts.css.ts
+++ src/layouts/Layouts.css.ts
@@ -3,12 +3,17 @@ import { style } from "@vanilla-extract/css"
 export const rootClass = style({
   minHeight: "100vh",
   display: "flex",
-  flexDirection: "column"
+  flexDirection: "column",
+  gap: "1.5rem"
 })
 
 export const headerClass = style({
   background: "#eee",
-  padding: "1.5rem 2rem"
+  padding: "1rem 2rem",
+  boxShadow: `0px 2.3px 3.6px rgba(0, 0, 0, 0.038),
+  0px 6.3px 10px rgba(0, 0, 0, 0.055),
+  0px 15.1px 24.1px rgba(0, 0, 0, 0.072),
+  0px 50px 80px rgba(0, 0, 0, 0.11)`
 })
 
 export const mainClass = style({
@@ -22,5 +27,5 @@ export const mainClass = style({
 export const footerClass = style({
   background: "#122774",
   color: "#fff",
-  padding: "1.5rem 2rem"
+  padding: "1rem 2rem"
 })
